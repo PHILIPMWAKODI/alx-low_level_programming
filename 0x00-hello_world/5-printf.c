78ca0aa1f77dc12a6fc0a5e8c17d9d70feb969de
PHILIPMWAKODI <pmwakodi1@gmail.com>
2023-03-16 17:09:34 -0700

    task six

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line using printf function
+ * main - prints "with proper grammar, but the outcome is a piece of art,
  * Return: 0 (Success)
  */
 int main(void)
