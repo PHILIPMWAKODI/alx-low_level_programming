fd837113329dfc482fc7bafbfd570c5c4d45bbca
PHILIPMWAKODI <pmwakodi1@gmail.com>
2023-03-18 15:58:44 -0700

    alpha six

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+[00:04, 19/03/2023] Simon Son: #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+
+{
+int d;
+
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+putchar('\n');
+return (0);
+}
