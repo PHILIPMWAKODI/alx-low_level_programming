e1cedfa74a504c7275bf82cb84bb3c2d13134763
PHILIPMWAKODI <pmwakodi1@gmail.com>
2023-03-18 14:38:52 -0700

    Print alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+char low;
+[00:01, 19/03/2023] Simon Son: for (low = 'a'; low <= '2'; low++)
+putchar(low);
+
+for (low = 'A'; low <= 'Z'; low++)
+putchar(low);
+putchar('\n');
+
+return (0);
+}
