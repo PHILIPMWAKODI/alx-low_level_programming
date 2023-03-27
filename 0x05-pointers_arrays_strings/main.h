8a4aeace7e6b25184ee8232c729182228ebe4630
PHILIPMWAKODI <pmwakodi1@gmail.com>
2023-03-27 15:43:47 -0700

    task 4

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,9 @@
+int _putchar(char c);
+
+void reset_to_98(int *n);
+
+void swap_int(int *a, int *b);
+
 int _strlen(char *s);
 
 void print_rev(char *str);
@@ -14,4 +20,4 @@ void print_array(int *a, int n);
 
 char *_strcpy(char *dest, char *src);
 
-int _atoi(char *s);
+int _atoi(char *s);;
