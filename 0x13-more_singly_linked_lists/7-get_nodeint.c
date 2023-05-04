bc1fcf211c1a88ed97647530c41abab63ae6ca4e
PHILIPMWAKODI <pmwakodi1@gmail.com>
2023-05-03 17:55:35 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -21,6 +21,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	}
 
-        if (i == index && current != NULL)
-        return current;
+       return (temp ? temp : NULL);
 }
